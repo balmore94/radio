f7d37697a2dc7ebd8e805678c50907a2c08fa043
tereaguillon <eliastere7@gmail.com>
2019-08-12 16:57:30 -0600

    medio dao

---
+++ radial/src/main/java/dao/EmisionesDao.java
@@ -0,0 +1,45 @@
+package dao;
+
+import conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.util.List;
+import modelo.EmisionesBean;
+
+public class EmisionesDao {
+    
+    Conexion conn;
+    
+    public EmisionesDao(Conexion con){
+        this.conn = conn; 
+    }
+    
+    public boolean insertar(EmisionesBean eb){
+        String sql = "insert into emisiones values (?,?)";
+        try {
+            PreparedStatement ps = conn.conectar().prepareStatement(sql);
+            ps.setInt(1, eb.getId_emisiones());
+            ps.setString(2, eb.getEmision());
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public boolean actualizar(EmisionesBean eb){
+        String sql = "update emisiones set emision=? where id_emisiones=?";
+        try {
+            PreparedStatement ps = conn.conectar().prepareStatement(sql);
+            ps.setString(1, eb.getEmision());
+            ps.setInt(2, eb.getId_emisiones());
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public List<EmisionesBean> consultarAll(){
+}
