bfdfb829505f378cde76177522e10e1a30edf152
merg8511 <merg8511@gmail.com>
2019-08-12 16:56:16 -0600

    Beans 16:56

---
+++ radial/src/main/java/modelo/Tipo_transmisionBean.java
@@ -0,0 +1,31 @@
+
+package modelo;
+
+
+public class Tipo_transmisionBean {
+    
+    private int id_transmision;
+    private String nombre_transmision;
+
+    public Tipo_transmisionBean(int id_transmision) {
+        this.id_transmision = id_transmision;
+    }
+    
+    public int getId_transmision() {
+        return id_transmision;
+    }
+
+    public void setId_transmision(int id_transmision) {
+        this.id_transmision = id_transmision;
+    }
+
+    public String getNombre_transmision() {
+        return nombre_transmision;
+    }
+
+    public void setNombre_transmision(String nombre_transmision) {
+        this.nombre_transmision = nombre_transmision;
+    }
+    
+    
+}
